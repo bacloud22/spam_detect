5346a57b370268b25411659ea72dadd2da09aa12
bacloud14 <bacloud14@gmail.com>
2021-12-26 12:46:10 +0100

    uniq client is ip+agent

--- server.js
+++ server.js
@@ -1,28 +1,39 @@
 const http = require('http')
 const axios = require('axios')
 const requestIp = require('request-ip')
+const { deepStrictEqual } = require('assert')
 
 const hostname = '127.0.0.1'
 const port = 3000
-const base = 3 // seconds
-const round = (seconds) => Math.floor(seconds / base) * base;
+// in seconds
+const rules = {
+    minDuration: 3600, // one hour
+    base: 3
+}
+const round = (seconds) => Math.floor(seconds / rules.base) * rules.base;
 const cache = {}
+function detect(timeSerie) {
+    // TODO: I don't know how
+    // Detect range cycles ? (always after rules.minDuration)
+    // Detect events happening daily on same time
+    // What could be the trends of a bot ?
+}
 
 const server = http.createServer((req, res) => {
     var t1 = new Date()
     t1.setMilliseconds(0)
     t1.setSeconds(round(t1.getSeconds()))
-    const clientIp = requestIp.getClientIp(req);
-    console.log(`request ip ${clientIp}`)
-    if( cache[clientIp] ) {
-        // (cache[clientIp].at(-1) !== t1)
-        if(cache[clientIp][cache[clientIp].length - 1].getTime() !== t1.getTime()) {
-            cache[clientIp].push(t1)
+    const client = requestIp.getClientIp(req)+'::'+req.headers['user-agent'];
+    if (cache[client]) {
+        // (cache[client].at(-1) !== t1)
+        if (cache[client][cache[client].length - 1].getTime() !== t1.getTime()) {
+            cache[client].push(t1)
         }
     } else {
-        cache[clientIp] = [t1]
+        cache[client] = [t1]
     }
     console.log(cache)
+    detect(cache[client])
     res.statusCode = 200
     res.setHeader('Content-Type', 'text/plain')
     res.end('Hello World')
